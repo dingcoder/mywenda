84f0d6482f1327b232c70ca9dd5e84cb6664d1da
ding <527663565@qq.com>
2018-12-14 16:32:53 +0800

    修改配置信息

--- src/main/java/pers/mywenda/MywendaApplication.java
+++ src/main/java/pers/mywenda/MywendaApplication.java
@@ -1,10 +1,14 @@
-package pers.mywenda.mywenda;
+package pers.mywenda;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MywendaApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(MywendaApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(MywendaApplication.class, args);
---
+++ src/test/java/pers/mywenda/MywendaApplicationTests.java
@@ -0,0 +1,19 @@
+package pers.mywenda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MywendaApplicationTests.class)
+@WebAppConfiguration
+public class MywendaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
